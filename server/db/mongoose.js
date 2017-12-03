fc68f12f3c84b3f4df05991e45f9e49859cc807d
Byron Ajin <byronap120@gmail.com>
2017-12-02 23:05:17 -0600

    fixing mongo URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var mongoURL = process.env.MONGODB_URI;
-mongoose.connect(mongoURL);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
