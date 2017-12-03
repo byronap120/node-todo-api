d5b8ad4f21b184d15fcd1066fc475b164fb4aa44
Byron Ajin <byronap120@gmail.com>
2017-12-02 22:47:38 -0600

    Set up configuration
    - adding config file for different environment
    - fixing package for test env variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var mongoURL = process.env.MONGODB_URI ||  'mongodb://localhost:27017/TodoApp';
+var mongoURL = process.env.MONGODB_URI;
 mongoose.connect(mongoURL);
 
 module.exports = {
