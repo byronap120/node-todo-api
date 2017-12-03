857fad0f40745897061ae5b95c32c7cfa61891b2
Byron Ajin <byronap120@gmail.com>
2017-12-02 23:26:14 -0600

    revert mongo URI conf

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-require('./config/config.js');
-
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,7 +8,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
