5121f8043157e7c1cc814864a4c829b65b05babd
Byron Ajin <byronap120@gmail.com>
2018-03-06 22:59:02 -0600

    unit test for users route
    - adding seed file to separate mock data
    
    - adding unit test for POST/users and  GET/users/me

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,53 @@
+const { ObjectID } = require('mongodb');
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+    _id: userOneId,
+    email: 'byron@example.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
+    }]
+}, {
+    _id: userTwoId,
+    email: 'jen@example.com',
+    password: 'userTwoPass',
+}];
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'fist test'
+}, {
+    _id: new ObjectID(),
+    text: 'second test',
+    copmleted: true,
+    completedAt: 333
+}];
+
+var populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+};
+
+var populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne, userTwo]);
+    }).then(() => done());
+};
+
+module.exports = {
+    populateTodos,
+    todos,
+    populateUsers,
+    users
+};
