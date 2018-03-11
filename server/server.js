99172e8a0c35e12ba376a70f62bef5f59a38d0fd
Byron Ajin <byronap120@gmail.com>
2018-03-10 18:25:25 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -96,6 +96,7 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
 app.post('/users', (req, res) => {
   var userRequest = _.pick(req.body, ['email', 'password']);
   var user = new User(userRequest);
@@ -107,6 +108,20 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+// POST /users/login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByUser(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(404).send();
+  });
+
 });
 
 app.get('/users/me', authenticate, (req, res) => {
